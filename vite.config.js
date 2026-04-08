72d7adbfa58e614cf25942b10f5f73ddbb59332c
jeriko001 <longo01fabio@gmail.com>
2026-04-08 13:24:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/ArcadeRoom/',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: false,
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-        }
-    })
   ]
 })
